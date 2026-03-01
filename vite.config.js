5303aa3e95e777dded03a09efe4e9f32466e8c02
onalimn48 <nicolas.j.carter@gmail.com>
2026-03-01 17:08:54 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/musical-caterpillar/',
   build: {
     outDir: 'dist',
     rollupOptions: {
