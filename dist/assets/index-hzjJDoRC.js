c9f8567ce417b9e24e642e6790418f8104050563
onalimn48 <nicolas.j.carter@gmail.com>
2026-03-12 14:21:22 -0500

    updated layout

--- dist/assets/index-hzjJDoRC.js
+++ dist/assets/index-hzjJDoRC.js
@@ -1,4 +1,4 @@
-import{r as j,j as x}from"./client-54u6xXdC.js";/**
+import{r as j,j as x}from"./client-CFaLfK-b.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
